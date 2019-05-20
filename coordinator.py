2ce0bf05db1b0f1bdc794ecb356f62667e01735a
tomas99batista <tomas99batista@gmail.com>
2019-05-20 18:17:45 +0100

    initial commit

---
+++ coordinator.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+
+import logging
+import argparse
+
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
+logger = logging.getLogger('coordinator')
+
+
+def main(args):
+    datastore = []
+    with args.file as f:
+        while True:
+            blob = f.read(args.blob_size)
+            if not blob:
+                break
+            # This loop is used to not break word in half
+            while not str.isspace(blob[-1]):
+                ch = f.read(1)
+                if not ch:
+                    break
+                blob += ch
+            logger.debug('Blob: %s', blob)
+            datastore.append(blob)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MapReduce Coordinator')
+    parser.add_argument('-p', dest='port', type=int, help='coordinator port', default=8765)
+    parser.add_argument('-f', dest='file', type=argparse.FileType('r'), help='file path')
+    parser.add_argument('-b', dest ='blob_size', type=int, help='blob size', default=1024)
+    args = parser.parse_args()
+    
+    main(args)
+
---
+++ worker.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+
+import logging
+import argparse
+
+
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
+logger = logging.getLogger('worker')
+
+
+def main(args):
+    logger.debug('Connecting to %s:%d', args.hostname, args.port)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MapReduce worker')
+    parser.add_argument('--port', dest='port', type=int, help='coordinator port', default=8765)
+    parser.add_argument('--hostname', dest='hostname', type=str, help='coordinator hostname', default='localhost')
+    args = parser.parse_args()
+    
+    main(args)
+
