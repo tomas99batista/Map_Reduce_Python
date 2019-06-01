c827ef38eb4d311925ecb81eac64fdfc4ffbff16
tomas99batista <tomas99batista@gmail.com>
2019-06-01 14:19:21 +0100

    memento ready 2 reverse

--- coordinator.py
+++ coordinator.py
@@ -1,14 +1,41 @@
 # coding: utf-8
-
-
+# Tomas Batista | 89296
+import socket
 import logging
 import argparse
+import selectors
+import types
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
 logger = logging.getLogger('coordinator')
 
+# Lista de sockets
+socks_list = {}
+
+# Selector
+sel = selectors.DefaultSelector()
+
+def accept(sock, mask):
+    conn, addr = sock.accept()  # Should be ready to read
+    logger.debug('accepted %s from %s', conn, addr)
+    conn.setblocking(False)
+    socks_list[addr[1]] = conn
+    logger.debug('socks_list: %s', socks_list)
+    sel.register(conn, selectors.EVENT_READ, read)
+
+def read(conn, mask):
+    data = conn.recv(1024)      # Should be ready
+    if data:
+        logger.debug('echoing %s to %s', repr(data), conn)
+        for port in socks_list.keys():
+            socks_list[port].send(data)
+    else:
+        logger.debug('closing %s', conn)
+        sel.unregister(conn)
+        conn.close()
 
 def main(args):
+    # Aqui vão ser criadas as blobs
     datastore = []
     with args.file as f:
         while True:
@@ -21,16 +48,30 @@ def main(args):
                 if not ch:
                     break
                 blob += ch
-            logger.debug('Blob: %s', blob)
+            #logger. debug('Blob: %s', blob)
             datastore.append(blob)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('0.0.0.0', args.port))
+    sock.listen(100)
+    sock.setblocking(False)
+    while True:
+        sel.register(sock, selectors.EVENT_READ,accept)
+        while True:
+            events = sel.select()
+            for key, mask in events:
+                callback = key.data
+                callback(key.fileobj, mask)
+
+    
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MapReduce Coordinator')
     parser.add_argument('-p', dest='port', type=int, help='coordinator port', default=8765)
-    parser.add_argument('-f', dest='file', type=argparse.FileType('r'), help='file path')
+    parser.add_argument('-f', dest='file', type=argparse.FileType('r'), help='file path', default='lusiadas.txt')
     parser.add_argument('-b', dest ='blob_size', type=int, help='blob size', default=1024)
     args = parser.parse_args()
     
     main(args)
-
--- worker.py
+++ worker.py
@@ -1,17 +1,63 @@
 # coding: utf-8
-
-
+# Tomas Batista | 89296
+import socket
 import logging
 import argparse
-
+import time
+import selectors
+import types
+import json
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
 logger = logging.getLogger('worker')
 
+# Function to Receive Data
+def receiveData(connection):
+    while True:
+        data = connection.recv(1024) # receive the response
+        logger.debug('Received %s', repr(data))
+        # Depois disto vai fazer work
 
+# Function to Process and Send Data
+def processData(stdin):
+    global s
+    msg = {
+        "TimeStamp" : time.strftime("%H:%M:%S"),
+        "Author" : "Root",
+        "Message" : "Ta a funcionar"
+    }
+    s.send(json.dumps(msg).encode('utf8'))
+
+def start_connections(host, port):
+        server_addr = (host, port)
+        print('starting connection to', server_addr)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        sock.send(b'Connected')
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(recv_total=0, outb=b'')
+        sel.register(sock, events, data=data)
+        
+# Main
 def main(args):
-    logger.debug('Connecting to %s:%d', args.hostname, args.port)
-
+        start_connections(args.hostname, args.port)
+        while True:
+            events = sel.select(timeout=None)
+            for key, mask in events:
+                sock = key.fileobj
+                data = key.data
+                if mask & selectors.EVENT_READ:
+                    recv_data = sock.recv(1024)  # Should be ready to read
+                    if recv_data:
+                        logger.debug('received %s', repr(recv_data))
+                    if not recv_data:
+                        sel.unregister(sock)
+                        sock.close()
+                if mask & selectors.EVENT_WRITE:
+                        #logger.debug('echoing %s to %s', repr(data), sock)
+                        # sock.send(b'toma')  # Should be ready to write
+                        pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MapReduce worker')
@@ -20,4 +66,3 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     main(args)
-
