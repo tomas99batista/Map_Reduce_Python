366da2a2b364ea8574d9559c4b746c6259151ee6
tomas99batista <tomas99batista@gmail.com>
2019-06-07 11:39:10 +0100

    added new texts, csv's and sh scripts

--- coordinator.py
+++ coordinator.py
@@ -61,11 +61,10 @@ def main(args):
     hist = []
     # store final histogram into a CSV file
     with args.out as f:
-        csv_writer = csv.writer(f, delimiter=',',
-        quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
+        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for w,c in hist:
             csv_writer.writerow([w,c])
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(('0.0.0.0', args.port))
@@ -82,8 +81,10 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MapReduce Coordinator')
     parser.add_argument('-p', dest='port', type=int, help='coordinator port', default=8765)
-    parser.add_argument('-f', dest='file', type=argparse.FileType('r'), help='file path', default='lusiadas.txt')
+    parser.add_argument('-f', dest='file', type=argparse.FileType('r', encoding='UTF-8'), help='input file path', default=)
+    parser.add_argument('-o', dest='out', type=argparse.FileType('w', encoding='UTF-8'), help='output file path', default='output.csv')
     parser.add_argument('-b', dest ='blob_size', type=int, help='blob size', default=1024)
     args = parser.parse_args()
+    
     
     main(args)
